d87ccb4a52ee78ddef08d418d1ff5cb366df2857
cassiea12 <c.amantite@gmail.com>
2017-09-24 20:49:42 -0700

    added portfolio and contactme section

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,3 +1,57 @@
+//LOADING SCREEN
+function onReady(callback) {
+    var intervalID = window.setInterval(checkReady, 1000);
+    function checkReady() {
+        if (document.getElementsByTagName('body')[0] !== undefined) {
+            window.clearInterval(intervalID);
+            callback.call(this);
+        }
+    }
+}
+
+function show(id, value) {
+    document.getElementById(id).style.display = value ? 'block' : 'none';
+}
+
+onReady(function () {
+    show('home', true);
+    show('about', true);
+    show('services', true);
+    show('portfolio', true);
+    show('contact', true);
+    show('loading', false);
+});
+
+// ANIMATION ELEMENTS
+
+var $animation_elements = $('.animation-element');
+var $window = $(window);
+$window.on('scroll', check_if_in_view);
+$window.on('scroll resize', check_if_in_view);
+$window.trigger('scroll');
+
+function check_if_in_view() {
+    var window_height = $window.height();
+    var window_top_position = $window.scrollTop();
+    var window_bottom_position = (window_top_position + window_height);
+
+    $.each($animation_elements, function() {
+        var $element = $(this);
+        var element_height = $element.outerHeight();
+        var element_top_position = $element.offset().top;
+        var element_bottom_position = (element_top_position + element_height);
+
+        //check to see if this current container is within viewport
+        if ((element_bottom_position >= window_top_position) &&
+            (element_top_position <= window_bottom_position)) {
+            $element.addClass('in-view');
+        } else {
+            $element.removeClass('in-view');
+        }
+    });
+}
+
+
 
 // ON SCROLL CHANGE NAVBAR STYLE
 $(document).ready(function () {
@@ -8,11 +62,15 @@ $(document).ready(function () {
         $(document).scroll(function () {
             scroll_start = $(this).scrollTop();
             if (scroll_start > offset.top) {
-                $(".navbar-light").css('background-color', 'rgba(255,255,255,.8)');
+                $(".navbar-light").css('background-color', 'rgba(255,255,255,1)');
                 $(".navbar-light .navbar-nav a").css('color', '#636363');
+                $(".navbar-collapse").css('background-color', 'transparent');
+                $(".navbar-collapse .navbar-nav a").css('color', '#636363');
             } else {
                 $('.navbar-light').css('background-color', 'transparent');
                 $(".navbar-light .navbar-nav a").css('color', 'white');
+                $('.navbar-collappse').css('background-color', 'rgba(255,255,255,1)');
+                $(".navbar-collapse .navbar-nav a").css('color', 'grey');
 
             }
         });
