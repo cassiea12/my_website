77240298f5c867003f69c703bcccd5019a441fd3
cassiea12 <c.amantite@gmail.com>
2017-09-29 17:25:52 -0700

    smaller file sizes

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,27 +1,3 @@
-// LOADING SCREEN
-function onReady(callback) {
-    var intervalID = window.setInterval(checkReady, 1000);
-    function checkReady() {
-        if (document.getElementsByTagName('body')[0] !== undefined) {
-            window.clearInterval(intervalID);
-            callback.call(this);
-        }
-    }
-}
-
-function show(id, value) {
-    document.getElementById(id).style.display = value ? 'block' : 'none';
-}
-
-onReady(function () {
-    show('home', true);
-    show('about', true);
-    show('services', true);
-    show('portfolio', true);
-    show('contact', true);
-    show('loading', false);
-});
-
 
 // ANIMATION ELEMENTS FADE IN UP
 var $animation_elements = $('.animation-element');
